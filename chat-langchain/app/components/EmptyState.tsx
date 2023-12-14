977bec545badcc675ca426f9499ae4f8fa1a7af2
Adil-Ashraf <adil.ashraf@devbox.co>
2023-12-14 18:31:13 +0500

    Change the text and the design of opening words on chatbot

--- chat-langchain/app/components/EmptyState.tsx
+++ chat-langchain/app/components/EmptyState.tsx
@@ -1,17 +1,33 @@
-import { MouseEvent, MouseEventHandler } from "react";
-import { Heading, Link, Card, CardHeader, Flex, Spacer } from "@chakra-ui/react";
-import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { MouseEvent } from 'react';
+import { Heading } from '@chakra-ui/react';
 
-export function EmptyState(props: {
-  onChoice: (question: string) => any
-}) {
-  const handleClick = (e: MouseEvent) => {
-    props.onChoice((e.target as HTMLDivElement).innerText);
-  }
+export function EmptyState({ onChoice }) {
+  const handleClick = (e: MouseEvent<HTMLDivElement>) => {
+    onChoice(e.target.innerText);
+  };
+
   return (
     <div className="p-8 rounded flex flex-col items-center">
-      <Heading fontSize="3xl" fontWeight={"medium"} mb={1} color={"white"}>Chat Devbox</Heading>
-      <Heading fontSize="xl" fontWeight={"normal"} mb={1} color={"white"} marginTop={"10px"} textAlign={"center"}>
+      <Heading
+        as="h1"
+        fontSize="4xl"
+        fontWeight="bold"
+        color="teal.300"
+        mb={4}
+        letterSpacing="wider"
+        _hover={{ color: "teal.400", cursor: "pointer" }}
+        onClick={handleClick}
+      >
+        Chat Devbox
+      </Heading>
+      <Heading
+        as="h2"
+        fontSize="2xl"
+        fontWeight="normal"
+        color="gray.300"
+        mb={4}
+        textAlign="center"
+      >
         How can I help you today?
       </Heading>
     </div>
